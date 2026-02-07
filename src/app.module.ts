071c0cc2d8acb648e6468cd8bd1b577bf2c4ff58
Cshu26 <chiewshuye@gmail.com>
2026-02-07 15:29:59 +0800

    change app.modules.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -42,11 +42,11 @@ import { Message } from './messages/entities/message.entity';
         entities: [User, Conversation, Message],
         synchronize: true, // previously, Set to false in production
         logging: configService.get<string>('NODE_ENV') === 'development',
-        options:{
+        // options:{
           // encrypt: true,  // Enforces TLS encryption for data in transit
           // trustServerCertificate: true // Certificate validation is disabled for development/demo environment due to self-signed cert (no CA-signed cert)
                                        // In production, this should be set to false and use a valid TLS certificate to prevent MITM attacks.
-        },
+        //},
       }),
       inject: [ConfigService],
     }),
